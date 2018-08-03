351625206de714d4cbe0a6e9627c65f2b277efe9
Jonathan Greene <Jonathangreene888@gmail.com>
2018-08-03 11:08:39 -0600

    Added some database settings

--- djangotest/djangotest/settings.py
+++ djangotest/djangotest/settings.py
@@ -83,14 +83,15 @@ WSGI_APPLICATION = 'djangotest.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'DEFAULT': dj_database_url.config(),
+    }
+}
 
-DATABASES['default'] =  dj_database_url.config()
+
 
 
 # Password validation
